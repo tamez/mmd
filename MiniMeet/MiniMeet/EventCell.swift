6a72ff128c212984d3b8ef7bd1e058d5f0c7f34e
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2015-03-14 13:55:05 -0700

    Add initial arrays and display table cells

---
+++ MiniMeet/MiniMeet/EventCell.swift
@@ -0,0 +1,29 @@
+//
+//  EventCell.swift
+//  MiniMeet
+//
+//  Created by Michelle Venetucci Harvey on 3/14/15.
+//  Copyright (c) 2015 hi_hu. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventLocation: UILabel!
+    @IBOutlet weak var eventTime: UILabel!
+    @IBOutlet weak var eventImage: UIImageView!
+       override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
