95e267ce2f1564f90cfe2081b99ff3eb1e3ee41b
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2015-03-15 20:59:12 -0700

    Add sign in flow and sign up process

--- MiniMeet/MiniMeet/CreateViewController.swift
+++ MiniMeet/MiniMeet/CreateViewController.swift
@@ -43,7 +43,29 @@ class CreateViewController: UIViewController, UIActionSheetDelegate {
     }
     
     @IBAction func submitButtonDidPress(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
+       // dismissViewControllerAnimated(true, completion: nil)
+        
+        //Create the AlertController
+        let actionSheetController: UIAlertController = UIAlertController(title: "Alert", message: "Swiftly Now! Choose an option!", preferredStyle: .Alert)
+        
+        //Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            //Do some stuff
+        }
+        actionSheetController.addAction(cancelAction)
+        //Create and an option action
+        let nextAction: UIAlertAction = UIAlertAction(title: "Next", style: .Default) { action -> Void in
+            //Do some other stuff
+        }
+        actionSheetController.addAction(nextAction)
+        //Add a text field
+        actionSheetController.addTextFieldWithConfigurationHandler { textField -> Void in
+            //TextField configuration
+            textField.textColor = UIColor.blueColor()
+        }
+        
+        //Present the AlertController
+        self.presentViewController(actionSheetController, animated: true, completion: nil)
     }
     
     @IBAction func addImageDidPress(sender: AnyObject) {
